5e7fd0ff5a26544708f6edb3e3c5210b1fd622eb
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-07-24 13:40:13 +0200

    read files async

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,27 @@ use tokio::io::AsyncBufReadExt;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let stdin = io::stdin();
-    let stdin = io::BufReader::new(stdin);
-    let mut count = 0u32;
-    let mut lines = stdin.lines();
-    while let Some(_) = lines.next_line().await? {
-        count += 1;
+    let mut args = std::env::args();
+    let _me = args.next();
+    let mut tasks = vec![];
+
+    for filename in args {
+        tasks.push(tokio::spawn(async {
+            let file = tokio::fs::File::open(filename).await?;
+            let file = io::BufReader::new(file);
+            let mut lines = file.lines();
+            let mut count = 0u32;
+            while let Some(_) = lines.next_line().await? {
+                count += 1;
+            }
+            Ok(count) as Result<u32, std::io::Error>
+        }));
     }
-    println!("Lines on stdin: {}", count);
+
+    let mut count = 0u32;
+    for task in tasks {
+        count += task.await??;
+    }
+    println!("Total lines: {}", count);
     Ok(())
 }
